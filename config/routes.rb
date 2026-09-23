19024704847d3144fb3ac3876eca35d332277e5c
Ryo712 <hiltoc.katsu@gmail.com>
2026-09-23 17:37:37 +0900

    ページの共通ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "top" => "home#top"
+  get "posts/index"
+  get "/posts/index" => "posts#index"
+  get "/" => "home#top"
   get "about" => "home#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
