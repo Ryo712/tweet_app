d9f58400bf7ecec810293e546501c69ef7edfbea
Ryo712 <hiltoc.katsu@gmail.com>
2026-09-24 00:30:58 +0900

    ポストに投稿時間を表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "posts/index"
   get "/posts/index" => "posts#index"
+  get "posts/:id" => "posts#show"
   get "/" => "home#top"
   get "about" => "home#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
